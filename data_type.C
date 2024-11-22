2553e9e572d439e1cd1092d4cd3f96e5012b74ee
Gulshid Zadae <gulshidzada4@gmail.com>
2024-11-22 11:20:01 +0500

    Data Type of C Language

---
+++ data_type.C
@@ -0,0 +1,23 @@
+#include <stdio.h>    
+#include<stdbool.h>                                     
+using namespace std;
+
+main(){
+    int a=10;
+    float b=1.2;
+    char c='Z';
+    char d[30]="Hi! This is String";
+    bool e=true;
+    printf("Displaying integer value data_type:%d",a);
+    printf("\n");
+    printf("Displaying float value data_type:%f",b);
+    printf("\n");
+    printf("Displaying char value data_type:%c",c);
+    printf("\n");
+    printf("Displaying String value data_type:%s",d);
+    printf("\n");
+     printf("Displaying boolean value data_type:%s\n",e?"true":"false");
+    printf("\n");
+    
+
+    }
