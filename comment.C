29310242a902b4dffa6ab0600b084d89f9dc4755
Gulshid Zadae <gulshidzada4@gmail.com>
2024-11-22 11:06:07 +0500

    Comment of C Language

---
+++ comment.C
@@ -0,0 +1,11 @@
+#include <stdio.h>                                         
+using namespace std;
+
+main(){
+ //Comment are those portion of programming which don't compile and it's used for other people who read the program to better understand 
+                //Single line comment
+
+                /*
+                multi_line Comment
+                */
+}
