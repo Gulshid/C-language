c33f8e6c1d1bf62f115751eda399688504edb087
Gulshid Zadae <gulshidzada4@gmail.com>
2024-11-24 18:10:44 +0500

    Arthematic Operator

---
+++ arthematic_operator.C
@@ -0,0 +1,22 @@
+#include<stdio.h>
+using namespace std;
+main(){
+    //Arthematic Operator
+    int x,y;
+     printf("Program Start...");
+    printf("Enter the two value to add:");
+    scanf("%d",&x);
+    scanf("%d",&y);
+    printf("The addition of two value is:%d",x+y);
+    printf("\n");
+    printf("The addition of two value is:%d",x-y);
+     printf("\n");
+    printf("The addition of two value is:%d",x*y);
+     printf("\n");
+    printf("The addition of two value is:%f",x / y);
+     printf("\n");
+     printf("Program End");
+      
+     
+
+}
