abb1be589835bcd4f5844d4f72a722c0c604940a
Gulshid Zadae <gulshidzada4@gmail.com>
2024-11-24 11:11:15 +0500

    This is ASCII code for all Characters

---
+++ find_ASCII.C
@@ -0,0 +1,10 @@
+#include<stdio.h>
+using namespace std;
+main(){
+    char ascii;
+    printf("Enter Any Character to find the ASCII code for that Character:");
+    scanf("%c",&ascii);
+    printf("***ASCII(Ammerican Standard code for international interchange)***");
+    printf("\n");
+    printf("The ASCII for this character is %d",ascii);
+}
