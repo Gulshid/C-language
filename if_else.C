441bb622492bc4633783be15384418f3b02ca2d5
Gulshid Zadae <gulshidzada4@gmail.com>
2024-11-24 18:42:13 +0500

    if else conditional statement

---
+++ if_else.C
@@ -0,0 +1,14 @@
+#include<stdio.h>
+using namespace std;
+main(){
+    int x=23;
+    if(x==0){
+        printf("The value of x is equal to zero ");
+    }
+    else if(x>0){
+        printf("The value of x is greater than zero");
+    }
+    else if(x<0){
+        printf("The value of x is less tha Zero");
+    }
+}
