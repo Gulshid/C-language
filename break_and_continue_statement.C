b3a96b4f23e8efe3a9d7d3edfc99e46765ba6ff6
Gulshid Zadae <gulshidzada4@gmail.com>
2024-11-25 12:34:59 +0500

    Break and continue statement

---
+++ break_and_continue_statement.C
@@ -0,0 +1,11 @@
+#include<stdio.h>
+using namespace std;
+main(){
+    for(int i=1; i<=10; i++){
+        if(i==4){
+             printf("%d",i);
+            continue;
+           
+        }
+    }
+    } 
