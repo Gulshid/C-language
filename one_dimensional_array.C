dacee02be4ac3be4fff3f4b6972055cad512bcb1
Gulshid Zadae <gulshidzada4@gmail.com>
2024-11-25 17:06:32 +0500

    One_dimensional Array

---
+++ one_dimensional_array.C
@@ -0,0 +1,14 @@
+#include<stdio.h>
+using namespace std;
+main(){
+    int d[2]={1,2};
+    printf("%d\t",d[0]);
+    printf("%d",d[1]);
+
+    printf("\n");
+    int c[10]={1,2,3,4,5,6,7,8,9,10};
+    for(int g=0; g<=9; g++){
+        printf("%d\t",c[g]);
+    }
+
+}
